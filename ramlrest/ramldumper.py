750b9fa61f5300d09a87d3a5b42e13f69f9d00a8
Bowe Strickland <bowe@redhat.com>
2017-10-28 14:43:51 -0400

    RAML checkpoint: working but imperfect RAML diagnostics

---
+++ ramlrest/ramldumper.py
@@ -0,0 +1,80 @@
+import logging; log = logging.getLogger(__file__)
+DEBUG = log.debug; INFO = log.info; WARN = log.warning; ERROR = log.error
+
+from attr import asdict as attr_asdict
+import ramlfications
+
+class RamlDumper(object):
+
+    klass = None             # subclass responsibility
+    REGISTRY=[]
+
+    omit_list = "config root parent".split()
+
+    @classmethod
+    def register(cls, subclass):
+        cls.REGISTRY.append((subclass.klass, subclass))
+        return cls
+
+    @classmethod
+    def get_dumper_for_object(cls, obj):
+        for match_class, klass in cls.REGISTRY:
+            if isinstance(obj, match_class):
+                return klass(obj)
+
+    @classmethod
+    def dump(cls, object):
+        d = cls.get_dumper_for_object(object)
+        return d.asdict() if d else object
+
+    def __init__(self, context):
+        self.context = context
+
+    def asdict(self):
+
+        def _gen_values(adict):
+
+            #import pdb; pdb.set_trace()
+            for key, value in adict.items():
+
+                if (not value) or (key in self.omit_list):
+                    continue
+
+                yield "_type", self.context.__class__.__name__
+
+                # note this forces all iterables to a list...
+                if hasattr(value, "__iter__") and not isinstance(value, str):
+                    yield key, [ self.dump(v) for v in value ]
+                else:
+                    yield key, self.dump(value)
+
+        d = attr_asdict(self.context, recurse=False)
+        return dict(_gen_values(d))
+
+@RamlDumper.register
+class RamlRootNodeDumper(RamlDumper):
+
+    klass = ramlfications.raml.RootNode
+    omit_list = RamlDumper.omit_list[:]
+    omit_list.remove('config')
+
+@RamlDumper.register
+class RamlBaseNodeDumper(RamlDumper):
+
+   klass = ramlfications.raml.BaseNode
+   omit_list = RamlDumper.omit_list + ['resources']
+
+@RamlDumper.register
+class RamlParameterDumper(RamlDumper):
+
+     klass = ramlfications.parameters.BaseParameter
+
+@RamlDumper.register
+class RamlBodyDumper(RamlDumper):
+
+     klass = ramlfications.parameters.Body
+
+@RamlDumper.register
+class RamlResponseDumper(RamlDumper):
+
+     klass = ramlfications.parameters.Response
--- ramlrest/ramlrest.py
+++ ramlrest/ramlrest.py
@@ -1,53 +1,82 @@
 import logging; log = logging.getLogger(__file__)
 DEBUG = log.debug; INFO = log.info; WARN = log.warning; ERROR = log.error
 
-from pyramid.response import Response
-import pyramid.httpexceptions as exc
+import yaml
+
+#from pyramid.response import Response
+#import pyramid.httpexceptions as exc
 
 import ramlfications
+
+from .ramldumper import RamlDumper
 
 
 class RamlResource(object):
 
-    def __init__(self, node):
+    def __init__(self, raml_node):
 
-        self.node = node
-        self.resource_map = {}
-        for r in getattr(node, 'resources', []):
-            # strip leading "/" from name
-            method_map = self.resource_map.setdefault(r.name[1:], {})
-            method_map[r.method.lower()] = RamlResource(r)
+        self.raml_node = raml_node
+        self.resource_map = \
+            {r.path:RamlResource(r) for r in raml_node.children}
 
     def __getitem__(self, key):
-        return self.resource_map[key]
+        return self.resource_map['/'+key]
+
+    def dump_tree(self):
+        print(self)
+        for k in sorted(self.resource_map):
+            print(k)
+            print(self.resource_map[k])
 
 class RootRamlResource(RamlResource):
 
     def __init__(self, api):
-        DEBUG("init root resource %s", api)
-        super(RootRamlResource, self).__init__(api)
         self.api = api
+        self._build_resource_children(api)
+        super(RootRamlResource, self).__init__(api)
+
+    def _build_resource_children(self, api):
+
+        def _init_children(n):
+            if not n.parent:
+                n.parent = api
+            n.children = []
+            return n
+
+        api.children = []
+        r_map = {n.path:_init_children(n) for n in api.resources}
+        for n in api.resources:
+            n.parent.children.append(n)
 
 def default_view(context, request):
+    return RamlDumper.dump(context.raml_node)
 
-    #import pdb; pdb.set_trace()
+class YAMLRenderer(object):
 
-    if isinstance(context, RootRamlResource):
-        return Response('hello root:%s' % context)
+    def __call__(self, info):
+        def _render(value, system):
+            request = system.get('request')
+            if request is not None:
+                response = request.response
+                ct = response.content_type
+                if ct == response.default_content_type:
+                    #response.content_type = 'application/yaml'
+                    response.content_type = 'text/plain'
+            return yaml.dump(value, default_flow_style=False)
 
-    r = context.get(request.method.lower())
-    if not r:
-        raise exc.HTTPBadRequest("method %s not allowed." % request.method)
+        return _render
 
-    return Response('hello world:%s' % r)
 
 def includeme(config):
     raml_file = config.get_settings()["ramlrest.raml_file"]
     api = ramlfications.parse(raml_file)
     root = RootRamlResource(api)
+    root.dump_tree()
 
     def root_factory(request):
         return root
 
+    config.add_renderer('yaml', YAMLRenderer())
+
     config.add_route('raml', '/raml/*traverse', factory=root_factory)
-    config.add_view(default_view, route_name='raml')
+    config.add_view(default_view, route_name='raml', renderer='yaml')
